4b5a5eaf7cad8a96d7efc407890d4caaf25f16f1
edwinkim97 <edwinkim124@gmail.com>
2022-02-17 14:39:39 -0800

    finished get/post companies

--- routes/companies.js
+++ routes/companies.js
@@ -6,6 +6,8 @@ const db = require("../db");
 
 const router = new express.Router();
 
+const { NotFoundError } = require("../expressError");
+
 router.get("/", async function (req, res){
 
     const results = await db.query(
@@ -13,7 +15,48 @@ router.get("/", async function (req, res){
         FROM companies`
     )
     const companies = results.rows;
-    return res.json({ companies })
+    return res.json({ companies });
+})
+
+router.get("/:code", async function (req, res){
+    const code = req.params.code
+
+    const results = await db.query(
+        `SELECT code, name, description
+        FROM companies
+        WHERE code = $1`, [code]
+    );
+
+    if (!results.rows[0]) {
+        throw new NotFoundError("Company Not Found");
+    }
+
+    const company = results.rows[0];
+    return res.json({ company });
+})
+
+router.post("/", async function (req, res){
+    const code = req.body.code;
+    const name = req.body.name;
+    const desc = req.body.description;
+
+    try{const results = await db.query(
+        `INSERT INTO companies 
+        VALUES ($1, $2, $3)
+        RETURNING code, name, description`,
+        [code, name, desc]
+    );} catch(error) {
+        console.log(error);
+        return res.json({ error: `${code} already exists.`})
+    }
+    
+
+    if (!results.rows[0]) {
+        throw new NotFoundError("Company Not Found");
+    }
+
+    const company = results.rows[0];
+    return res.json({ company });
 })
 
 module.exports = router;
